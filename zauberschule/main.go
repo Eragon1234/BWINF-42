d0c5adc89cfeed7ea045e026101c0587ee2cadef
Alexander Gerstberger <a.gerstberger@web.de>
2023-10-23 10:47:41 +0200

    adding main.go for zauberschule that reads a given filepath and writes it back

---
+++ zauberschule/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: zauberschule <filepath>")
+		return
+	}
+
+	filepath := os.Args[1]
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
