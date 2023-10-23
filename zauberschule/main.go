7b25a4be4eba088e24dcc69ddc2293bde703786f
Alexander Gerstberger <a.gerstberger@web.de>
2023-10-23 13:46:20 +0200

    parsing the plan, the start and the end coordinate from the input file

--- zauberschule/main.go
+++ zauberschule/main.go
@@ -40,4 +40,50 @@ func main() {
 		slice.New2D[bool](n, m),
 		slice.New2D[bool](n, m),
 	}
+
+	var start, end [3]int
+
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		for j, c := range scanner.Text() {
+			if c == 'A' {
+				start = [3]int{0, i, j}
+			} else if c == 'B' {
+				end = [3]int{0, i, j}
+			}
+			plan[0][i][j] = c != '#'
+		}
+	}
+
+	// skip the empty line
+	scanner.Scan()
+
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		for j, c := range scanner.Text() {
+			if c == 'A' {
+				start = [3]int{1, i, j}
+			} else if c == 'B' {
+				end = [3]int{1, i, j}
+			}
+
+			plan[1][i][j] = c != '#'
+		}
+	}
+
+	fmt.Println(start, end)
+
+	for _, s := range plan {
+		for _, row := range s {
+			for _, c := range row {
+				if c {
+					fmt.Print(".")
+				} else {
+					fmt.Print("#")
+				}
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
 }
