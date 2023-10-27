990dcd4f5a7f4bb76396728b96b5638c8d4ebb25
Alexander Gerstberger <a.gerstberger@web.de>
2023-10-27 21:31:51 +0200

    Added documentation to slice and heap

--- pkg/heap/heap.go
+++ pkg/heap/heap.go
@@ -8,6 +8,10 @@ type Heap[T any] struct {
 	h *internalHeap
 }
 
+// New returns a new heap with the given less function.
+//
+// If the less function returns true,
+// the first element will be popped before the second.
 func New[T any](less func(T, T) bool) *Heap[T] {
 	return &Heap[T]{
 		h: &internalHeap{
@@ -18,14 +22,17 @@ func New[T any](less func(T, T) bool) *Heap[T] {
 	}
 }
 
+// Push adds x to the heap.
 func (h *Heap[T]) Push(x T) {
 	heap.Push(h.h, x)
 }
 
+// Pop removes and returns the minimum element (according to less).
 func (h *Heap[T]) Pop() T {
 	return heap.Pop(h.h).(T)
 }
 
+// Len returns the number of elements in the heap.
 func (h *Heap[T]) Len() int {
 	return h.h.Len()
 }
--- pkg/slice/dimensional.go
+++ pkg/slice/dimensional.go
@@ -1,5 +1,6 @@
 package slice
 
+// New2D returns a slice containing n slices of length m.
 func New2D[T any](n, m int) [][]T {
 	s := make([][]T, n)
 	for i := range s {
