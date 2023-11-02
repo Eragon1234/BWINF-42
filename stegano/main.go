bd8751d0a642b8d5ee5b08aaead676c7be1a9475
Alexander Gerstberger <a.gerstberger@web.de>
2023-11-02 14:29:11 +0100

    Added main.go with working version for St. Egano

---
+++ stegano/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: stegano <filepath>")
+		return
+	}
+
+	filepath := os.Args[1]
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	img, err := png.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var result string
+
+	bounds := img.Bounds()
+
+	var x, y int
+
+	for {
+		r, g, b, _ := img.At(x, y).RGBA()
+		r >>= 8
+		g >>= 8
+		b >>= 8
+		x += int(g)
+		y += int(b)
+		for x >= bounds.Max.X {
+			x -= bounds.Max.X
+		}
+		for y >= bounds.Max.Y {
+			y -= bounds.Max.Y
+		}
+		result += string(r)
+
+		if g == 0 && b == 0 {
+			break
+		}
+	}
+
+	fmt.Println(result)
+}
